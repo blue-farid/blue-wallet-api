1d6d7d4374ae4f9a3a7ec1a688ec6d44fcec181f
faird.masjedi <farid.masjedi>
2025-10-26 18:22:32 +0330

    fix: get transaction response accessors

--- src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/response/GetTransactionsResponse.java
+++ src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/response/GetTransactionsResponse.java
@@ -2,9 +2,11 @@ package com.snapp.pay.insurance.bluewallet.api.v1.response;
 
 import com.snapp.pay.insurance.bluewallet.api.v1.model.TransactionDto;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.data.domain.Page;
 
 @Data
+@Accessors(chain = true)
 public class GetTransactionsResponse {
     private Page<TransactionDto> transactions;
 }
