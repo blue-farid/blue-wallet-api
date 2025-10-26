41bac667f9d167a8041fcf7cf2cb6cb430b9edc8
faird.masjedi <farid.masjedi>
2025-10-26 15:03:15 +0330

    initial commit

---
+++ src/test/java/com/snapp/pay/insurance/bluewallet/api/BlueWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapp.pay.insurance.bluewallet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
